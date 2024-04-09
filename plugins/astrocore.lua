23cd8d9123b616a04157f5fcf943ba077c8fa584
bfahrenfort <bfahrenfort1@gmail.com>
2024-04-09 13:03:06 -0500

    bugfix

--- plugins/astrocore.lua
+++ plugins/astrocore.lua
@@ -32,11 +32,11 @@ return {
         ["<Leader>bh"] = { "<cmd>bprev<cr>", desc = "Previous buffer" },
         ["<Leader>bl"] = { "<cmd>bnext<cr>", desc = "Next buffer" },
         L = {
-          function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+          function() require("astrocore.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
           desc = "Next buffer",
         },
         H = {
-          function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+          function() require("astrocore.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
           desc = "Previous buffer",
         },
         J = {
